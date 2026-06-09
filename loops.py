e0f0aee75cede609a0c67389a3cbdc7b2aa70ce8
Ashni <ashnisermaraja1104@gmail.com>
2026-06-09 15:44:22 +0530

    Add looping exercises and FizzBuzz

---
+++ loops.py
@@ -0,0 +1,55 @@
+print("1. Multiplication Table")
+print("2. Sum 1-100")
+print("3. FizzBuzz")
+print("4. Number Guessing Game")
+
+choice = int(input("Enter your choice (1-4): "))
+
+if choice == 1:
+    num = int(input("Enter a number: "))
+
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
+
+elif choice == 2:
+    total = 0
+
+    for i in range(1, 101):
+        total += i
+
+    print("Sum =", total)
+
+elif choice == 3:
+    for i in range(1, 51):
+
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+
+        elif i % 3 == 0:
+            print("Fizz")
+
+        elif i % 5 == 0:
+            print("Buzz")
+
+        else:
+            print(i)
+
+elif choice == 4:
+    target = 25
+
+    guess = int(input("Guess the number: "))
+
+    while guess != target:
+
+        if guess < target:
+            print("Higher!")
+
+        else:
+            print("Lower!")
+
+        guess = int(input("Try again: "))
+
+    print("Congratulations! You guessed correctly.")
+
+else:
+    print("Invalid choice")
